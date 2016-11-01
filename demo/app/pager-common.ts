a0ca93589f8e5e913e9addf2eee95770554e0e3b
Osei Fortune <triniwiz@Oseis-Mac.local>
2016-11-01 13:47:55 -0400

    IOS Update

---
+++ demo/app/pager-common.ts
@@ -0,0 +1,42 @@
+import { PropertyMetadataSettings, Property, PropertyChangeData } from "ui/core/dependency-observable";
+import { PropertyMetadata } from "ui/core/proxy";
+import { View } from "ui/core/view";
+export module knownCollections {
+    export var items = "items";
+}
+
+function onSelectedIndexChanged(data: PropertyChangeData) {
+    const pager = <Pager>data.object;
+    pager.updateIndex(pager.selectedIndex);
+}
+
+function onItemsChanged(data: PropertyChangeData) {
+    const pager = <Pager>data.object;
+    pager.updateItems(pager.items);
+}
+export class Pager extends View {
+    private _disableSwipe: boolean;
+    public static selectedIndexProperty = new Property("selectedIndex", "Pager", new PropertyMetadata(undefined, PropertyMetadataSettings.None, onSelectedIndexChanged));
+    public static itemsProperty = new Property("items", "Pager", new PropertyMetadata(undefined, PropertyMetadataSettings.None, onItemsChanged));
+
+    get items() {
+        return this._getValue(Pager.itemsProperty);
+    }
+    set items(value: Array<any>) {
+        this._setValue(Pager.itemsProperty, value);
+    }
+    get selectedIndex() {
+        return this._getValue(Pager.selectedIndexProperty);
+    }
+    set selectedIndex(value: number) {
+        this._setValue(Pager.selectedIndexProperty, value);
+    }
+    get disableSwipe(): boolean {
+        return this._disableSwipe;
+    }
+    set disableSwipe(value: boolean) {
+        this._disableSwipe = value;
+    }
+
+
+}    
--- demo/app/pager.ts
+++ demo/app/pager.ts
@@ -1,159 +1,193 @@
 import { StackLayout } from "ui/layouts/stack-layout";
-import { PropertyMetadataSettings, Property, PropertyChangeData } from "ui/core/dependency-observable";
-import { PropertyMetadata } from "ui/core/proxy";
-declare var com;
-function onSelectedIndexChanged(data: PropertyChangeData) {
-    const item = <Pager>data.object;
-    item.updateIndex(item.selectedIndex);
-}
-export class Pager extends StackLayout {
-    private _androidViewId: number;
-    private _pagerAdapter: android.support.v4.view.PagerAdapter;
+import { View, AddArrayFromBuilder } from "ui/core/view";
+import * as view from "ui/core/view";
+import * as utils from "utils/utils";
+import { Image } from "ui/image";
+import * as common from "./pager-common";
+declare var ADTransitioningDelegate, ADCubeTransition, ADTransitioningViewController;
+global.moduleMerge(common, exports);
+export class Pager extends common.Pager implements AddArrayFromBuilder {
+
+    private _orientation: UIPageViewControllerNavigationOrientation;
+    private _options: NSDictionary<any, any>;
+    private _views: Array<any>;
     private _transformer;
-    private _android: android.support.v4.view.ViewPager;
-    public static selectedIndexProperty = new Property("selectedIndex", "Pager", new PropertyMetadata(undefined, PropertyMetadataSettings.None));
+    private _ios: UIPageViewController;
+    get views() {
+        return this._views;
+    }
     constructor() {
         super();
+        this._views = [];
+        const that = new WeakRef(this);
+        this._orientation = UIPageViewControllerNavigationOrientation.Horizontal;
+        this._transformer = UIPageViewControllerTransitionStyle.Scroll;
+        const nsVal = <any>[0.0]
+        const nsKey = <any>[UIPageViewControllerOptionInterPageSpacingKey];
+        this._options = NSDictionary.dictionaryWithObjectsForKeys(nsKey, nsVal);
+        this._ios = UIPageViewController.alloc().initWithTransitionStyleNavigationOrientationOptions(this._transformer, this._orientation, this._options);
     }
-    get android() {
-        return this._android;
-    }
-    get _nativeView() {
-        return this._android;
-    }
-    get pagerAdapter() {
-        return this._pagerAdapter;
-    }
-    set transformer(value) {
-        switch (value) {
-            case "AccordionTransformer":
-                this._transformer = com.eftimoff.viewpagertransformers.AccordionTransformer;
-                break;
-            case "BackgroundToForegroundTransformer":
-                this._transformer = com.eftimoff.viewpagertransformers.BackgroundToForegroundTransformer;
-                break;
-            case "CubeInTransformer":
-                this._transformer = com.eftimoff.viewpagertransformers.CubeInTransformer;
-                break;
-            case "CubeOutTransformer":
-                this._transformer = com.eftimoff.viewpagertransformers.CubeOutTransformer;
-                break;
-            case "DefaultTransformer":
-                this._transformer = com.eftimoff.viewpagertransformers.DefaultTransformer;
-                break;
-            case "DepthPageTransformer":
-                this._transformer = com.eftimoff.viewpagertransformers.DepthPageTransformer;
-                break;
-            case "DrawFromBackTransformer":
-                this._transformer = com.eftimoff.viewpagertransformers.DrawFromBackTransformer;
-                break;
-            case "FlipHorizontalTransformer":
-                this._transformer = com.eftimoff.viewpagertransformers.FlipHorizontalTransformer;
-                break;
-            case "FlipVerticalTransformer":
-                this._transformer = com.eftimoff.viewpagertransformers.FlipVerticalTransformer;
-                break;
-            case "ForegroundToBackgroundTransformer":
-                this._transformer = com.eftimoff.viewpagertransformers.ForegroundToBackgroundTransformer;
-                break;
-            case "RotateDownTransformer":
-                this._transformer = com.eftimoff.viewpagertransformers.RotateDownTransformer;
-                break;
-            case "RotateUpTransformer":
-                this._transformer = com.eftimoff.viewpagertransformers.RotateUpTransformer;
-                break;
-            case "StackTransformer":
-                this._transformer = com.eftimoff.viewpagertransformers.StackTransformer;
-                break;
-            case "TabletTransformer":
-                this._transformer = com.eftimoff.viewpagertransformers.TabletTransformer;
-                break;
-            case "ZoomInTransformer":
-                this._transformer = com.eftimoff.viewpagertransformers.ZoomInTransformer;
-                break;
-            case "ZoomOutSlideTransformer":
-                this._transformer = com.eftimoff.viewpagertransformers.ZoomOutSlideTransformer;
-                break;
-            case "ZoomOutTranformer":
-                this._transformer = com.eftimoff.viewpagertransformers.ZoomOutTranformer;
-                break;
+
+
+    public _addArrayFromBuilder(name: string, value: Array<any>) {
+        if (name === "items") {
+            this.items = value;
         }
     }
+
     get transformer() {
         return this._transformer;
     }
-    get selectedIndex() {
-        return this._getValue(Pager.selectedIndexProperty);
+    get ios(): UIView {
+        return this._ios.view;
     }
-    set selectedIndex(value: number) {
-        this._setValue(Pager.selectedIndexProperty, value);
-    }
-    _createUI() {
-        if (!this._androidViewId) {
-            this._androidViewId = android.view.View.generateViewId();
-        };
-        this._android = new android.support.v4.view.ViewPager(this._context);
-        this._android.setId(this._androidViewId);
+    get _nativeView(): UIView {
+        return this._ios.view;
     }
 
-    onLoaded() {
+    public onLoaded() {
         super.onLoaded();
+        if (!this.selectedIndex) {
+            this.selectedIndex = 0;
+        }
         const that = new WeakRef(this);
-        this._android.setOffscreenPageLimit(this._android.getChildCount());
-        this._pagerAdapter = new PagerAdapter(this);
-        const PagerListener = java.lang.Object.extend({
-            interfaces: [android.support.v4.view.ViewPager.OnPageChangeListener],
-            onPageSelected: function (position: number) {
-                that.get().selectedIndex = position;
-            },
-            onPageScrolled: function (position, positionOffset, positionOffsetPixels) {
-            },
-            onPageScrollStateChanged: function (state) {
-            }
-        });
-        this._android.setOnPageChangeListener(new PagerListener());
-        this._android.setAdapter(this._pagerAdapter);
-        if (this.transformer) {
-            this._android.setPageTransformer(false, new this._transformer());
-        }
-        if (this.selectedIndex) {
-            this._android.setCurrentItem(this.selectedIndex);
-        }
+        this._ios.dataSource = PagerDataSource.initWithOwner(that);
+        this._ios.delegate = PagerViewControllerDelegate.initWithOwner(that);
+        let controller = this.addViewController(this.selectedIndex);
+        let dir = UIPageViewControllerNavigationDirection.Forward;
+        this._ios.setViewControllersDirectionAnimatedCompletion(<any>[controller], dir, false, () => { });
+
     }
 
-    updateIndex(index: number) {
-        if (this._android && index) {
-            this._android.setCurrentItem(index);
+    public updateIndex(index: number) {
+
+    }
+    public updateItems(val) { }
+    public runUpdate() {
+    }
+    public addViewController(selectedIndex: number): UIViewController {
+        const that = new WeakRef(this);
+        return this.items[selectedIndex].ios.nextResponder;
+       // let vc: PagerView = new PagerView();
+       // vc.tag = selectedIndex;
+       // vc.owner = that.get();
+        //vc.view.addSubview(this.items[selectedIndex].ios)
+       // vc.view.addSubview(item);
+       // return vc;
+    }
+
+    public onMeasure(widthMeasureSpec: number, heightMeasureSpec: number): void {
+        
+      //  View.adjustChildLayoutParams(this.items[this.selectedIndex], widthMeasureSpec, heightMeasureSpec);
+
+        var result = View.measureChild(this, this.items[this.selectedIndex], widthMeasureSpec, heightMeasureSpec);
+
+        var width = utils.layout.getMeasureSpecSize(widthMeasureSpec);
+        var widthMode = utils.layout.getMeasureSpecMode(widthMeasureSpec);
+
+        var height = utils.layout.getMeasureSpecSize(heightMeasureSpec);
+        var heightMode = utils.layout.getMeasureSpecMode(heightMeasureSpec);
+
+        var density = utils.layout.getDisplayDensity();
+        var measureWidth = Math.max(result.measuredWidth, this.minWidth * density);
+        var measureHeight = Math.max(result.measuredHeight, this.minHeight * density);
+
+        var widthAndState = View.resolveSizeAndState(measureWidth, width, widthMode, 0);
+        var heightAndState = View.resolveSizeAndState(measureHeight, height, heightMode, 0);
+
+        this.setMeasuredDimension(widthAndState, heightAndState);
+    }
+    public onLayout(left: number, top: number, right: number, bottom: number): void {
+        View.layoutChild(this, this.items[this.selectedIndex], 0, 0, right - left, bottom - top);
+
+      //  View.restoreChildOriginalParams(this.items[this.selectedIndex]);
+    }
+
+}
+
+class PagerViewControllerDelegate extends NSObject implements UIPageViewControllerDelegate {
+    public static ObjCProtocols = [UIPageViewControllerDelegate];
+    private _owner: WeakRef<Pager>;
+    get owner(): Pager {
+        return this._owner.get();
+    }
+    public static initWithOwner(owner: WeakRef<Pager>): PagerViewControllerDelegate {
+        let pv = new PagerViewControllerDelegate();
+        pv._owner = owner;
+        return pv;
+    }
+    pageViewControllerDidFinishAnimatingPreviousViewControllersTransitionCompleted(pageViewController: UIPageViewController, finished: boolean, previousViewControllers: NSArray<any>, completed: boolean) {
+        if (finished) {
+            let vc = <PagerView>pageViewController.viewControllers[0];
+            let pos = vc.tag;
+            this.owner.selectedIndex = pos;
         }
     }
 }
-Pager.selectedIndexProperty.onValueChanged = onSelectedIndexChanged;
-class PagerAdapter extends android.support.v4.view.PagerAdapter {
-    private owner: Pager;
-    constructor(owner) {
-        super();
-        this.owner = owner;
-        return global.__native(this);
+class PagerDataSource extends NSObject implements UIPageViewControllerDataSource {
+    public static ObjCProtocols = [UIPageViewControllerDataSource];
+    private _owner: WeakRef<Pager>;
+    get owner(): Pager {
+        return this._owner.get();
     }
-
-    instantiateItem(collection: android.view.ViewGroup, position: number) {
-        if (position <= this.owner.android.getChildCount() - 1) {
-            const view = this.owner.android.getChildAt(position);
-            collection.removeView(this.owner.android.getChildAt(position));
-            collection.addView(view, position);
-            this.owner.pagerAdapter.notifyDataSetChanged();
-            return view;
+    public static initWithOwner(owner: WeakRef<Pager>): PagerDataSource {
+        let ds = new PagerDataSource();
+        ds._owner = owner;
+        return ds;
+    }
+    pageViewControllerViewControllerBeforeViewController(pageViewController: UIPageViewController, viewControllerBefore: UIViewController): UIViewController {
+        let vc = <PagerView>viewControllerBefore;
+        let pos = vc.tag;
+        if (pos === 0) {
+            return null;
+        } else {
+            let prev = pos - 1;
+            return this.owner.addViewController(prev);
+            //return this.owner.items[prev].ios;
+        }
+    }
+    pageViewControllerViewControllerAfterViewController(pageViewController: UIPageViewController, viewControllerAfter: UIViewController): UIViewController {
+        let vc = <PagerView>viewControllerAfter;
+        let pos = vc.tag;
+        let count = this.presentationCountForPageViewController(pageViewController);
+        if (pos === count - 1) {
+            return null;
+        } else {
+            let next = pos += 1;
+           // console.log(this.owner.items[next].ios.view)
+           // return this.owner.items[next].ios.view;
+             return this.owner.addViewController(next);
         }
 
     }
+    presentationCountForPageViewController(pageViewController: UIPageViewController): number {
+        if (!this.owner) {
+            return 0;
+        }
+        return this.owner.items.length;
+    }
+    presentationIndexForPageViewController(pageViewController: UIPageViewController): number {
+        return this.owner.selectedIndex;
+    }
+}
 
-    destroyItem(container: android.view.ViewGroup, position: number, object) {
-        container.removeView(object);
+export class PagerView extends UIViewController {
+    public owner: any;
+    public tag: number;
+    public viewDidLoad() {
+        super.viewDidLoad();
+        if (this.owner) {
+            this.owner.onLoaded();
+        }
     }
-    getCount(): number {
-        return this.owner.android.getChildCount();
-    }
-    isViewFromObject(view: android.view.View, object) {
-        return view == object;
-    }
+    // public viewDidLayoutSubviews(): void {
+    //     if (this.owner) {
+    //        // var width = this.view.frame.size.width;
+    //        // var height = this.view.frame.size.height;
+    //        var width = 500;
+    //        var height = 500;
+    //         this.owner.measure(width, height);
+    //         this.owner.layout(0, 0, width, height);
+    //     }
+    // }
 }
