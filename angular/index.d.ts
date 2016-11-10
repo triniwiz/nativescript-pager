438b812dffb5e33d7bd1cdabc5b5a6945cf18c71
Osei Fortune <fortune.osei@yahoo.com>
2016-11-09 22:00:46 -0400

    Fix NgModule export

--- angular/index.d.ts
+++ angular/index.d.ts
@@ -1,20 +1,2 @@
-import { ElementRef, TemplateRef, ViewContainerRef } from "@angular/core";
-import { Pager } from "../pager";
-export declare class PagerDirective {
-    private element;
-    pager: Pager;
-    private _selectedIndex;
-    private viewInitialized;
-    selectedIndex: number;
-    constructor(element: ElementRef);
-    ngAfterViewInit(): void;
-}
-export declare class PagerItemDirective {
-    private owner;
-    private templateRef;
-    private viewContainer;
-    private item;
-    constructor(owner: PagerDirective, templateRef: TemplateRef<any>, viewContainer: ViewContainerRef);
-    private ensureItem();
-    ngOnInit(): void;
+export declare class PagerModule {
 }
