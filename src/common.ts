f3de6677775e9f84c0c61c567967e6b4d3bc2db8
Daniel Freiling <dfg@nota.dk>
2017-02-16 12:31:10 +0100

    refactor: use Property.onSetNative callback and changed abstract methods

--- src/common.ts
+++ src/common.ts
@@ -2,24 +2,31 @@ import { PropertyMetadataSettings, Property, PropertyChangeData } from "ui/core/
 import { PropertyMetadata } from "ui/core/proxy";
 import { View, AddArrayFromBuilder } from "ui/core/view";
 import { ContentView } from "ui/content-view";
+import * as types from "utils/types";
+
 export module knownCollections {
     export const items = "items";
 }
 
-function onSelectedIndexChanged(data: PropertyChangeData) {
-    const pager = <Pager>data.object;
-    pager.updateIndex(pager.selectedIndex);
-}
-
 function onItemsChanged(data: PropertyChangeData) {
     const pager = <Pager>data.object;
-    pager.updateItems(<Array<View>>data.oldValue, <Array<View>>data.newValue);
+    if (data.newValue) {
+        pager.updateNativeItems(<Array<View>>data.oldValue, <Array<View>>data.newValue);
+    }
 }
+
+function onSelectedIndexChanged(data: PropertyChangeData) {
+    const pager = <Pager>data.object;
+    if (pager && pager.items && types.isNumber(data.newValue)) {
+        pager.updateNativeIndex(data.oldValue, data.newValue);
+    }
+}
+
 export abstract class Pager extends View implements AddArrayFromBuilder {
     private _disableSwipe: boolean;
     private _pageSpacing: number = 0;
-    public static selectedIndexProperty = new Property("selectedIndex", "Pager", new PropertyMetadata(undefined, PropertyMetadataSettings.None, onSelectedIndexChanged));
-    public static itemsProperty = new Property("items", "Pager", new PropertyMetadata(undefined, PropertyMetadataSettings.None, onItemsChanged));
+    public static selectedIndexProperty = new Property("selectedIndex", "Pager", new PropertyMetadata(0, PropertyMetadataSettings.None, null, null, onSelectedIndexChanged));
+    public static itemsProperty = new Property("items", "Pager", new PropertyMetadata(undefined, PropertyMetadataSettings.AffectsLayout, null, null, onItemsChanged));
 
     public _addArrayFromBuilder(name: string, value: Array<any>) {
         if (name === "items") {
@@ -50,9 +57,9 @@ export abstract class Pager extends View implements AddArrayFromBuilder {
     set pageSpacing(value: number) {
         this._pageSpacing = value;
     }
-    public abstract updateItems(oldItems: Array<View>, newItems: Array<View>): void;
+    public abstract updateNativeItems(oldItems: Array<View>, newItems: Array<View>): void;
 
-    public abstract updateIndex(index: number): void;
+    public abstract updateNativeIndex(oldIndex: number, newIndex: number): void;
 }
 
 export abstract class PagerItem extends View { }
