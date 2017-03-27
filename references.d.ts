174251d93ef0cff5c6cb8130ab9aea61cb74c300
Osei Fortune <fortune.osei@yahoo.com>
2017-03-27 10:07:04 -0400

    tic fixes

---
+++ references.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="./node_modules/tns-core-modules/tns-core-modules.d.ts" /> Needed for autocompletion and compilation.
+/// <reference path="./node_modules/tns-core-modules/lib.core.d.ts" />
+/// <reference path="./node_modules/nativescript-angular/index.d.ts" />
+/// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" /> Needed for autocompletion and compilation.
+/// <reference path="./node_modules/tns-platform-declarations/android.d.ts" /> Needed for autocompletion and compilation.
