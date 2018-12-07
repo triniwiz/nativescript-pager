7370864a2c03df0fef0e2f0c0477d4c411e88c50
Osei Fortune <fortune.osei@yahoo.com>
2018-12-07 16:28:39 -0400

    fix(): ios building

--- src/pager.d.ts
+++ src/pager.d.ts
@@ -1,1 +1,26 @@
-export * from './pager.android';
+import { View } from 'tns-core-modules/ui/core/view';
+import { PagerBase } from './pager.common';
+
+export type Orientation = 'horizontal' | 'vertical';
+
+export declare class Pager extends PagerBase {
+    cache: boolean;
+
+    itemTemplateUpdated(oldData: any, newData: any): void;
+
+    readonly android: any;
+    readonly ios: any;
+
+    eachChildView(callback: (child: View) => boolean): void;
+
+    updateNativeIndex(oldIndex: number, newIndex: number): void;
+
+    updateNativeItems(oldItems: View[], newItems: View[]): void;
+
+    refresh(): void;
+
+    disableSwipe: boolean;
+    disableAnimation: boolean;
+
+    requestLayout(): void;
+}
