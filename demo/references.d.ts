a6807c6aaf6edbfe7b087c0d258315c690657778
Daniel Leal <danielgek@gmail.com>
2019-07-19 22:24:29 +0100

    fix ts-lint

--- demo/references.d.ts
+++ demo/references.d.ts
@@ -1,2 +1,4 @@
+/* tslint:disable */
 /// <reference path="../src/node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="../src/node_modules/tns-platform-declarations/android.d.ts" />
+/* tslint:enable */
