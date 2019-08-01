61f13c71743cadeecccc854057935efbb9712f1e
Osei Fortune <fortune.osei@yahoo.com>
2019-08-01 14:17:54 -0400

    chore(android): use latest sdk typings

--- demo/references.d.ts
+++ demo/references.d.ts
@@ -1,4 +1,4 @@
 /* tslint:disable */
 /// <reference path="../src/node_modules/tns-platform-declarations/ios.d.ts" />
-/// <reference path="../src/node_modules/tns-platform-declarations/android.d.ts" />
+/// <reference path="../src/node_modules/tns-platform-declarations/android-28.d.ts" />
 /* tslint:enable */
